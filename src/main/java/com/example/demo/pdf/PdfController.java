950a86204653d16eaa2f71d10b8f56c941d1e02c
jangjeonghun1004 <jangjeonghun1004@gmail.com>
2024-11-15 19:09:40 +0900

    init: commit

--- src/main/java/com/example/demo/pdf/PdfController.java
+++ src/main/java/com/example/demo/pdf/PdfController.java
@@ -1,10 +1,10 @@
 package com.example.demo.pdf;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import jakarta.servlet.http.HttpServletResponse;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
